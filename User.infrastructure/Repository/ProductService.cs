dd3e1e1a89f637fda0e442b9d6fb731831170287
NithishCIDC <nithish.sakthivelmurugan@concertidc.com>
2024-12-18 13:08:33 +0530

    .

--- User.infrastructure/Repository/ProductService.cs
+++ User.infrastructure/Repository/ProductService.cs
@@ -1,12 +1,4 @@
-﻿using Microsoft.Identity.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Http.Json;
-using Microsoft.VisualBasic;
+﻿using System.Net.Http.Json;
 using User.Application.DTO;
 using User.Application.Interface;
 
